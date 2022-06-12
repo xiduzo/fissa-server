f99d9e27cc8858f4bc99380790f41215dfb7a4af
Sander <mail@sanderboer.nl>
2022-06-12 19:33:29 +0200

    added vercel node

---
+++ src/api/token.ts
@@ -0,0 +1,63 @@
+import {VercelApiHandler} from '@vercel/node';
+import SpotifyWebApi from 'spotify-web-api-node';
+
+// app.get('/api', (req, res) => {
+//   res.send(
+//     JSON.stringify({
+//       hi: 'there',
+//     }),
+//   );
+// });
+
+// app.post('/api/token', async (req, res) => {
+//   const spotifyApi = new SpotifyWebApi(credentials);
+//   const response = await spotifyApi.authorizationCodeGrant(req.body.code);
+
+//   res.send(JSON.stringify(response.body));
+// });
+
+// app.post('/api/token/refresh', async (req, res) => {
+//   const spotifyApi = new SpotifyWebApi(credentials);
+//   spotifyApi.setAccessToken(req.body.access_token);
+//   spotifyApi.setRefreshToken(req.body.refresh_token);
+
+//   const response = await spotifyApi.refreshAccessToken();
+//   res.send(JSON.stringify(response.body));
+// });
+
+const credentials = {
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+};
+
+const handleAccessToken: VercelApiHandler = async (req, res) => {
+  const spotifyApi = new SpotifyWebApi(credentials);
+  const response = await spotifyApi.authorizationCodeGrant(req.body.code);
+  res.send(JSON.stringify(response.body));
+};
+
+const handleRefreshToken: VercelApiHandler = async (req, res) => {
+  const spotifyApi = new SpotifyWebApi(credentials);
+  spotifyApi.setAccessToken(req.body.access_token);
+  spotifyApi.setRefreshToken(req.body.refresh_token);
+  const response = await spotifyApi.refreshAccessToken();
+  res.send(JSON.stringify(response.body));
+};
+
+const handler: VercelApiHandler = (req, res) => {
+  switch (req.method) {
+    case 'GET':
+      res.send(
+        JSON.stringify({
+          hi: 'there',
+        }),
+      );
+      break;
+    case 'POST':
+      if (req.url?.endsWith('/token')) handleAccessToken(req, res);
+      if (req.url?.endsWith('/token/refresh')) handleRefreshToken(req, res);
+      break;
+  }
+};
+
+export default handler;
