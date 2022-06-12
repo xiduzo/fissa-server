5495100c5fc7263896ffe57813ab33b55c549d1e
Sander <mail@sanderboer.nl>
2022-06-12 18:52:37 +0200

    routing

--- src/api/token.ts
+++ src/api/token.ts
@@ -3,13 +3,16 @@ import express from 'express';
 import {clientErrorHandler, credentials, errorHandler, logErrors} from '..';
 
 const app = express();
+
+const tokenRoutes = express.Router();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(logErrors);
 app.use(clientErrorHandler);
 app.use(errorHandler);
+app.use('/api/token', tokenRoutes);
 
-app.get('/', async (req, res) => {
+tokenRoutes.get('/', async (req, res) => {
   res.send(
     JSON.stringify({
       hi: 'there',
@@ -17,14 +20,14 @@ app.get('/', async (req, res) => {
   );
 });
 
-app.post('/', async (req, res) => {
+tokenRoutes.post('/', async (req, res) => {
   const spotifyApi = new SpotifyWebApi(credentials);
   const response = await spotifyApi.authorizationCodeGrant(req.body.code);
 
   res.send(JSON.stringify(response.body));
 });
 
-app.post('/refresh', async (req, res) => {
+tokenRoutes.post('/refresh', async (req, res) => {
   const spotifyApi = new SpotifyWebApi(credentials);
   spotifyApi.setAccessToken(req.body.access_token);
   spotifyApi.setRefreshToken(req.body.refresh_token);
