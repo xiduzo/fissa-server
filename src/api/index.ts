19a5867827c481c93bbddfb7643f7af69e7149b2
Sander <mail@sanderboer.nl>
2022-06-12 19:14:16 +0200

    ??

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,54 +1,37 @@
-// import SpotifyWebApi from 'spotify-web-api-node';
-// import express from 'express';
-// import {clientErrorHandler, credentials, errorHandler, logErrors} from '..';
-
-// export const app = express();
-
-// app.use(express.json());
-// app.use(express.urlencoded({extended: true}));
-// app.use(logErrors);
-// app.use(clientErrorHandler);
-// app.use(errorHandler);
-
-// app.get('/api', async (req, res) => {
-//   res.send(
-//     JSON.stringify({
-//       hi: 'there',
-//     }),
-//   );
-// });
-
-// app.post('/api', async (req, res) => {
-//   const spotifyApi = new SpotifyWebApi(credentials);
-//   const response = await spotifyApi.authorizationCodeGrant(req.body.code);
-
-//   res.send(JSON.stringify(response.body));
-// });
-
-// app.post('/api/token/refresh', async (req, res) => {
-//   const spotifyApi = new SpotifyWebApi(credentials);
-//   spotifyApi.setAccessToken(req.body.access_token);
-//   spotifyApi.setRefreshToken(req.body.refresh_token);
-
-//   const response = await spotifyApi.refreshAccessToken();
-//   res.send(JSON.stringify(response.body));
-// });
-
-// export default app;
+import SpotifyWebApi from 'spotify-web-api-node';
 import express from 'express';
+import {clientErrorHandler, credentials, errorHandler, logErrors} from '..';
 
-const app = express();
+export const app = express();
 
-app.get('/api', (req, res) => {
-  const path = `/api/item/1234`;
-  res.setHeader('Content-Type', 'text/html');
-  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(logErrors);
+app.use(clientErrorHandler);
+app.use(errorHandler);
+
+app.get('/api', async (req, res) => {
+  res.send(
+    JSON.stringify({
+      hi: 'there',
+    }),
+  );
 });
 
-app.get('/api/item/:slug', (req, res) => {
-  const {slug} = req.params;
-  res.end(`Item: ${slug}`);
+app.post('/api', async (req, res) => {
+  const spotifyApi = new SpotifyWebApi(credentials);
+  const response = await spotifyApi.authorizationCodeGrant(req.body.code);
+
+  res.send(JSON.stringify(response.body));
+});
+
+app.post('/api/token/refresh', async (req, res) => {
+  const spotifyApi = new SpotifyWebApi(credentials);
+  spotifyApi.setAccessToken(req.body.access_token);
+  spotifyApi.setRefreshToken(req.body.refresh_token);
+
+  const response = await spotifyApi.refreshAccessToken();
+  res.send(JSON.stringify(response.body));
 });
 
 export default app;
