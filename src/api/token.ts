b238222372a1105ca827cccee6021895bb6fd4fa
Sander <mail@sanderboer.nl>
2022-06-12 18:02:52 +0200

    add api support

---
+++ src/api/token.ts
@@ -0,0 +1,23 @@
+import express from 'express';
+import SpotifyWebApi from 'spotify-web-api-node';
+import {credentials} from '..';
+
+const tokenRouter = express.Router();
+
+tokenRouter.post('/', async (req, res) => {
+  const spotifyApi = new SpotifyWebApi(credentials);
+  const response = await spotifyApi.authorizationCodeGrant(req.body.code);
+
+  res.send(JSON.stringify(response.body));
+});
+
+tokenRouter.post('/refresh', async (req, res) => {
+  const spotifyApi = new SpotifyWebApi(credentials);
+  spotifyApi.setAccessToken(req.body.access_token);
+  spotifyApi.setRefreshToken(req.body.refresh_token);
+
+  const response = await spotifyApi.refreshAccessToken();
+  res.send(JSON.stringify(response.body));
+});
+
+export default tokenRouter;
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-import express, {Request, Response, NextFunction} from 'express';
+import express, {NextFunction, Request, Response} from 'express';
 import actuator from 'express-actuator';
-import SpotifyWebApi from 'spotify-web-api-node';
 import http from 'http';
 import https from 'https';
+import tokenRouter from './api/token';
 
-const credentials = {
+export const credentials = {
   clientId: process.env.CLIENT_ID ?? 'a2a88c4618324942859ce3e1f888b938',
   clientSecret: process.env.CLIENT_SECRET ?? 'bfce3e5d96074c21ac4db8b4991c2f37',
   redirectUri: 'com.fissa:/oauth',
@@ -43,24 +43,10 @@ app.use(logErrors);
 app.use(clientErrorHandler);
 app.use(errorHandler);
 
+app.use('/api/token', tokenRouter);
+
 const server = http.createServer(app);
 const serverHttps = https.createServer(app);
-
-app.post('/api/token', async (req, res) => {
-  const spotifyApi = new SpotifyWebApi(credentials);
-  const response = await spotifyApi.authorizationCodeGrant(req.body.code);
-
-  res.send(JSON.stringify(response.body));
-});
-
-app.post('/api/refresh', async (req, res) => {
-  const spotifyApi = new SpotifyWebApi(credentials);
-  spotifyApi.setAccessToken(req.body.access_token);
-  spotifyApi.setRefreshToken(req.body.refresh_token);
-
-  const response = await spotifyApi.refreshAccessToken();
-  res.send(JSON.stringify(response.body));
-});
 
 const port = process.env.NODE_PORT ?? process.env.PORT ?? 8080;
 const portHttps = process.env.HTTPS_PORT ?? 8443;
