99b2f677f5b647fb4e424040631dee95974d2484
Sander <mail@sanderboer.nl>
2022-11-02 22:47:28 +0100

    try more times to connect to collection

--- src/store/_Store.ts
+++ src/store/_Store.ts
@@ -2,21 +2,19 @@ import { Collection, Document } from "mongodb";
 import { mongoCollection } from "../utils/database";
 
 export class Store<T> {
-  private _name = "Store";
   // @ts-ignore-next-line
   protected collection: Collection<T & Document>;
 
   constructor(name: string) {
-    this._name = name;
     mongoCollection<T>(name).then((collection) => {
       this.collection = collection;
     });
   }
 
   waitForCollection = async () => {
-    const TIMEOUTS = [500, 1000, 2000, 5000, 7500];
+    const TIMEOUTS = [500, 1000, 1000, 2000, 2000, 2000, 5000, 5000, 5000];
     let attempt = 0;
-    while (!this.collection && attempt < 5) {
+    while (!this.collection && attempt < TIMEOUTS.length - 1) {
       await new Promise((resolve) => setTimeout(resolve, TIMEOUTS[attempt]));
       attempt++;
     }
