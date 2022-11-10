86826db801b4377c6f56cddfcb28c41c46871edb
Sander <mail@sanderboer.nl>
2022-11-10 09:23:48 +0100

    added graph

--- src/lib/classes/errors/BadRequest.ts
+++
@@ -1,8 +0,0 @@
-import { ReasonPhrases, StatusCodes } from "http-status-codes";
-import { FissaError } from "./_FissaError";
-
-export class BadRequest extends FissaError {
-  constructor(message: string) {
-    super(message, StatusCodes.BAD_REQUEST, ReasonPhrases.BAD_REQUEST);
-  }
-}
