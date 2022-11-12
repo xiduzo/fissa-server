652525bf570454905df89712fcb9327957fc7da8
Sander <mail@sanderboer.nl>
2022-11-12 16:59:37 +0100

    fix order issue after voting for track above current track

--- src/processes/sync-track-order.ts
+++ src/processes/sync-track-order.ts
@@ -15,6 +15,7 @@ import { publish } from "../utils/mqtt";
 import { VoteService } from "../service/VoteService";
 import { TrackService } from "../service/TrackService";
 import { FissaError } from "../lib/classes/errors/_FissaError";
+import { RoomService } from "../service/RoomService";
 
 const TRACK_ORDER_SYNC_TIME = 1000 * 2;
 const NO_SYNC_MARGIN = 1000 * 5;
@@ -25,9 +26,8 @@ export const syncTrackOrder = async (appCache: cache) => {
   const promises = rooms?.map(
     async (room): Promise<void> =>
       new Promise(async (resolve) => {
-        const { accessToken, expectedEndTime, currentIndex, pin } = room;
+        const { expectedEndTime, currentIndex, pin } = room;
         try {
-          if (!accessToken) return;
           if (currentIndex < 0) return;
 
           const tMinus = DateTime.fromISO(
@@ -70,69 +70,58 @@ const reorderPlaylist = async (room: Room): Promise<number> => {
   try {
     const voteService = new VoteService();
     const trackService = new TrackService();
+    const roomService = new RoomService();
 
     const { pin, currentIndex } = room;
     const votes = await voteService.getVotes(pin);
     const sortedVotes = getScores(votes);
     const tracks = await trackService.getTracks(pin);
     const currentTrackId = tracks[currentIndex].id;
+
     const roomTracks = await mongoCollection<Track>("track");
     const voteIds = votes.map((vote) => vote.trackId);
 
     // 1 remove voted tracks from new order
-    let newTracksOrder = tracks.filter((track) => {
-      // Keep the current track
-      if (track.index === currentIndex) return true;
-      // Remove all tracks which have been voted on
-      return !voteIds.includes(track.id);
-    });
+    let newTracksOrder = tracks.filter((track) => !voteIds.includes(track.id));
+    const newCurrentTrackIndex = newTracksOrder.findIndex(
+      (track) => track.id === currentTrackId
+    );
 
-    // 2 add positive tracks
+    // // 2 add positive tracks
     const positiveVotes = sortedVotes.filter(positiveScore).sort(highToLow);
     if (positiveVotes.length) {
-      newTracksOrder = [
-        ...newTracksOrder.slice(0, currentIndex + 1),
-        ...mapTo(tracks, positiveVotes),
-        ...newTracksOrder.slice(currentIndex + 1),
-      ];
+      newTracksOrder = newTracksOrder
+        .slice(0, newCurrentTrackIndex + 1)
+        .concat(mapTo(tracks, positiveVotes))
+        .concat(newTracksOrder.slice(newCurrentTrackIndex + 1));
     }
 
-    // 3 add negative tracks at the end of the playlist
+    // // 3 add negative tracks at the end of the playlist
     const negativeVotes = sortedVotes.filter(negativeScore).sort(highToLow);
     if (negativeVotes.length) {
-      newTracksOrder = [...newTracksOrder, ...mapTo(tracks, negativeVotes)];
+      newTracksOrder = newTracksOrder.concat(mapTo(tracks, negativeVotes));
     }
 
-    // TODO: take into account when the a track moved from before the current index
     // 4 reorder playlist
     let reorders = 0;
     const promises = newTracksOrder.map(async (track, index) => {
-      const originalIndex = tracks.findIndex(
-        (original) => original.id === track.id
-      );
-      if (originalIndex === index) return;
+      const original = tracks.find((t) => t.index === index);
+
+      if (original?.id === track.id) return;
 
       reorders++;
-      logger.info(
-        `${pin}: ${track.name} (${track.id}) ${originalIndex} -> ${index}`
-      );
+      logger.info(`${pin}: set ${track.name} to index ${index}`);
 
       await roomTracks.updateOne({ pin, id: track.id }, { $set: { index } });
     });
 
     await Promise.all(promises);
 
-    const newCurrentTrackIndex = newTracksOrder.findIndex(
-      (track) => track.id === currentTrackId
-    );
-
-    if (newCurrentTrackIndex !== currentIndex) {
-      await publish(`fissa/room/${pin}`, {
-        ...room,
-        currentIndex: newCurrentTrackIndex,
-      });
+    if (newCurrentTrackIndex !== currentIndex || reorders) {
+      await roomService.updateRoom(room, newCurrentTrackIndex);
+      if (reorders) logger.info(`${pin} - ${reorders} reorders`);
     }
-    if (reorders) logger.info(`${pin} - ${reorders} reorders`);
+
     return reorders;
   } catch (error) {
     if (error instanceof FissaError) {
